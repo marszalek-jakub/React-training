f2295f7f42eec21bae29efbda7f8653a5346a4c5
Jakub Marszalek <marszalek.m.jakub@gmail.com>
2021-10-06 12:33:18 +0200

    add button logic to show hide component

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -2,16 +2,15 @@ import React from 'react';
 import Button from './Button';
 
 
-const Header = ({title}) => {
-    const onClick = () => {
-      console.log('click');
-    }
+const Header = ({title, onAdd, showAdd}) => {
+
     return(
         <header className="header">
             {/* <h1 style={headingStyle}>{title}</h1> */}
 
             <h1>{title}</h1>
-            <Button onClick={onClick} color="green" text="Add"/>
+            <Button onClick={onAdd} color={showAdd ? 'red' : 'green'} 
+            text={showAdd ? 'Close' : 'Add'}/>
         </header>
     )
 };

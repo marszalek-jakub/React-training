c866fb2fd9b06d6d976cf666a89316d5e13ed646
Jakub Marszalek <marszalek.m.jakub@gmail.com>
2022-09-30 21:47:33 +0200

    init Testify

---
+++ taskify/src/components/TodoList.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Todo } from './model'
+import SingleTodo from './SingleTodo'
+
+interface Props {
+    todos: Todo[]
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+const TodoList = ({ todos, setTodos }: Props) => {
+    return (
+        <div className="todos">
+            {todos.map(todo => (
+                <li><SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} /></li>
+            ))}
+        </div>
+    )
+}
+
+export default TodoList

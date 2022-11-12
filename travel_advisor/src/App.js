195a6d5eddd61f30a24bc777b014c27115ae0902
Jakub Marszalek <marszalek.m.jakub@gmail.com>
2022-11-12 23:53:24 +0100

    add fetching function, List component, Map component

--- travel_advisor/src/App.js
+++ travel_advisor/src/App.js
@@ -1,24 +1,40 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
 
-import './App.css';
-import React from 'react';
-import { CssBaseline, Grid } from '@material-ui/core';
+import { getPlacesData } from "./api";
 
-
-import Header from './components/Header/Header';
-import List from './components/List/List';
-import Map from './components/Map/Map';
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 function App() {
+  const [places, setPlaces] = useState([]);
+
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+
+  useEffect(() => {
+    getPlacesData().then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, []);
+
   return (
     <>
       <CssBaseline />
       <Header />
-      <Grid container spacing={3} style={{ width: '100%' }}>
+      <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
